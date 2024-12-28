73c73816238af68f3317335b08437b2311c606e7
Alan Daniels <alan.daniels@hotmail.com>
2024-12-28 18:04:26 -0500

    Add Firebase.

--- index.ts
+++ index.ts
@@ -6,3 +6,20 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0VmqD08KTLKKxLEiAcvF-6xnup0-L0mY",
+    authDomain: "chill-app-dev.firebaseapp.com",
+    projectId: "chill-app-dev",
+    storageBucket: "chill-app-dev.firebasestorage.app",
+    messagingSenderId: "1075707557290",
+    appId: "1:1075707557290:web:2ab9ff7f92bdb263be67d3",
+    measurementId: "G-ZE5KQW5JLY"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
